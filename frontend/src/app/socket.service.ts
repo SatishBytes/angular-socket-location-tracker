2e8edad8a5d76228c65b6560b9499bde5f21ed55
Satish Yadav <satishyadav42571@gmail.com>
2025-07-08 18:25:45 +0530

    Update socket.service.ts

--- frontend/src/app/socket.service.ts
+++ frontend/src/app/socket.service.ts
@@ -9,7 +9,7 @@ export class SocketService {
   private socket: Socket;
 
   constructor() {
-    this.socket = io('http://localhost:3000);
+    this.socket = io('http://localhost:3000');
   }
 
   sendLocation(data: { coords: { lat: number; lng: number }; name: string }) {
